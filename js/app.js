8ec73fb044df7c03ec668aa66bf565414e89e29c
Andrew Rogala <adrogala87@gmail.com>
2018-05-16 18:27:07 -0400

    create a random deck of 16 cards

--- js/app.js
+++ js/app.js
@@ -47,6 +47,43 @@ function shuffle(array) {
     return array;
 }
 
+let playRandomDeck = true;
+if(playRandomDeck === true) {
+    /*random deck*/
+    const randomDeck = ['fa-crow', 'fa-dove', 'fa-feather', 'fa-frog', 'fa-kiwi-bird', 'fa-broadcast-tower',
+    'fa-church', 'fa-hospital', 'fa-school', 'fa-university', 'fa-compass', 'fa-dollar-sign', 'fa-gift',
+    'fa-heart', 'fa-leaf', 'fa-parachute-box', 'fa-piggy-bank', 'fa-seedling', 'fa-poo', 'fa-smile', 'fa-frown',
+    'fa-chess', 'fa-chess-bishop', 'fa-chess-king', 'fa-chess-knight', 'fa-chess-pawn', 'fa-chess-queen',
+    'fa-chess-rook', 'fa-bug', 'fa-coffee', 'fa-keyboard', 'fa-user-secret', 'fa-microchip', 'fa-money-bill-alt',
+    'fa-bell', 'fa-clock', 'fa-hourglass-half', 'fa-stopwatch', 'fa-paint-brush', 'fa-paper-plane', 'fa-wrench',
+    'fa-hand-spock', 'fa-thumbs-up', 'fa-ambulance', 'fa-beer', 'fa-home', 'fa-anchor', 'fa-bicycle',
+    'fa-fighter-jet', 'fa-bed', 'fa-birthday-cake', 'fa-bomb', 'fa-blind', 'fa-glass-martini', 'fa-flag-checkered',
+    'fa-eye', 'fa-car', 'fa-helicopter', 'fa-gamepad', 'fa-graduation-cap', 'fa-motorcycle', 'fa-paw', 'fa-rocket',
+    'fa-music', 'fa-subway', 'fa-binoculars', 'fa-cube', 'fa-dice', 'fa-moon', 'fa-snowflake', 'fa-quidditch',
+    'fa-futbol', 'fa-football-ball', 'fa-user-astronaut', 'fa-user-ninja', 'fa-bus'];
+    //shuffle random deck
+    shuffle(randomDeck);
+
+    const randomCards = [];
+    const randomCardTemp = [];
+    //push 8 random cards from randomDeck onto randomCards
+    for(let i = 0; i<8; i++) {
+        randomCards.push(randomDeck[i]);
+    }
+
+    //double the card names by adding all 8 randomCards to a temp array
+    //then add all 8 cards from the temp arry onto to 8 rendomCards
+    for(let i = 0; i < 8; i++) {
+        randomCardTemp.push(randomCards[i]);
+        randomCards.push(randomCardTemp[i]);
+    }
+    //shuffle the randomCards
+    shuffle(randomCards);
+    console.log(randomCardTemp);
+    console.log(randomCards);
+}
+
+
 /*
  * Create a list that holds all of your cards
  * Select all 16 card list item elements (querySelectorAll returns a NodeList object)
