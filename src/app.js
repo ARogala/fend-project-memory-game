5f99e8963c3477f042a09bddf06d12f7268e997d
ARogala <adrogala87@gmail.com>
2019-04-11 16:59:32 -0400

    factor out winner function

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { StopWatchController } from './stopWatchModule';
 import { processSettings, processSettingsInit } from './processSettings';
 import { starRating } from './starRating';
 import { cheat } from './cheat';
+import { winner } from './winner.js';
 import './css/app.css';
 import './css/responsive.css';
 //variables
@@ -159,7 +160,7 @@ document.getElementById('playAgain').addEventListener('click', function() {
 document.getElementById('cheatBtn').addEventListener('click', runCheat);
 //must set moveCounter to 2 once we cheat
 function runCheat() {
-   moveCounter = cheat(moveCounter, startTimer, cheatCount, cardElements, moves)
+    moveCounter = cheat(moveCounter, startTimer, cheatCount, cardElements, moves);
 }
 
 //nav rules
@@ -223,58 +224,6 @@ function incrementMoveCounter() {
 
 function incrementMatchCounter() {
     matchCounter = matchCounter + 1;
-}
-
-/*
-Remove header score panel and deck from display
-add winner class to container thus displaying the winner background
-set winnerMsg class display to initial bringing it into view
-add total moves to results list element
-get the time and add the time to results list element
-call starRating function to add star rating to winner page
-*/
-function winner() {
-    header[0].style.cssText = 'display: none';
-    cheatElement[0].style.cssText = 'display: none';
-    scorePanel[0].style.cssText = 'display: none';
-    deck[0].style.cssText = 'display: none';
-    nav[0].style.cssText = 'display: none';
-    container[0].classList.add('winner');
-    winnerMsg[0].style.cssText = 'display: initial';
-    results[0].textContent = 'Total Moves: ' + moveCounter;
-    let minutes = minutesElement.innerHTML;
-    let seconds = secondsElement.innerHTML;
-    let deciSeconds = deciSecondsElement.innerHTML;
-    results[1].textContent = 'Total Time: ' + minutes + ' ' + seconds + ' ' + deciSeconds;
-    starAmount = starRating(moveCounter, starAmount, stars);
-    //local storage
-    //moves
-    if (sessionStorage.getItem('thisMove') === null) {
-        sessionStorage.setItem('thisMove', moveCounter);
-    } else if (sessionStorage.getItem('thisMove') !== null) {
-        sessionStorage.setItem('previousMove', sessionStorage.getItem('thisMove'));
-        sessionStorage.setItem('thisMove', moveCounter);
-    }
-    //time
-    if (sessionStorage.getItem('thisTime') === null) {
-        sessionStorage.setItem('thisTime', minutes + ' ' + seconds + ' ' + deciSeconds);
-    } else if (sessionStorage.getItem('thisTime') !== null) {
-        sessionStorage.setItem('previousTime', sessionStorage.getItem('thisTime'));
-        sessionStorage.setItem('thisTime', minutes + ' ' + seconds + ' ' + deciSeconds);
-    }
-    //Stars
-    if (sessionStorage.getItem('thisStars') === null) {
-        sessionStorage.setItem('thisStars', starAmount);
-    } else if (sessionStorage.getItem('thisStars') !== null) {
-        sessionStorage.setItem('previousStars', sessionStorage.getItem('thisStars'));
-        sessionStorage.setItem('thisStars', starAmount);
-    }
-    //if any previous value not null update UI
-    if (sessionStorage.getItem('previousStars') !== null) {
-        previousResults[0].textContent = 'Total Moves: ' + sessionStorage.getItem('previousMove');
-        previousResults[1].textContent = 'Total Time: ' + sessionStorage.getItem('previousTime');
-        previousResults[2].textContent = 'Star Rating: ' + sessionStorage.getItem('previousStars');
-    }
 }
 
 // Nav menu functions
@@ -444,7 +393,26 @@ function runTheGame(e) {
         //delay winner allowing user to see the last move
         if (matchCounter === 8) {
             StopWatchController.stopStopWatch();
-            setTimeout(winner, 1500);
+            setTimeout(
+                winner(
+                    header,
+                    cheatElement,
+                    scorePanel,
+                    deck,
+                    nav,
+                    container,
+                    winnerMsg,
+                    results,
+                    moveCounter,
+                    minutesElement,
+                    secondsElement,
+                    deciSecondsElement,
+                    starAmount,
+                    stars,
+                    previousResults
+                ),
+                1500
+            );
         }
     }
 }
