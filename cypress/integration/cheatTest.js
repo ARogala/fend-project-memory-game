66daeb5dc7de2a7f51ac63eb85d773e3e6691c16
ARogala <adrogala87@gmail.com>
2019-04-16 19:11:16 -0400

    test cheat function

---
+++ cypress/integration/cheatTest.js
@@ -0,0 +1,28 @@
+describe('Cheat function', function() {
+	it('Shows all cards for 2 sec', function() {
+		cy.visit('/');
+		cy.clock();
+		cy.get('#cheatBtn')
+			.click()
+			.then(() => {
+				cy.get('.card').then(cardElements => {
+					for (const cardElement of cardElements) {
+						expect(cardElement.classList.value).to.equal('card show open');
+					}
+				});
+			});
+		cy.tick(2500);
+		cy.get('.card').then(cardElements => {
+			for (const cardElement of cardElements) {
+				expect(cardElement.classList.value).to.equal('card');
+			}
+		});
+	});
+
+	it('Increments Moves by 2', function() {
+		cy.get('.moves').then(movesElement => {
+			let moves = parseInt(movesElement[0].innerHTML);
+			expect(moves).to.equal(2);
+		});
+	});
+});
